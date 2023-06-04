764ed9c26b98d3a72b09b1711c7c7732040e5f57
boydpelley <boydjrpelley@gmail.com>
2023-06-04 18:14:23 -0300

    still working with relative links

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       </div>
       <div id="media">
         <h1 className="sectionTitle">media</h1>
-        <img src="../assets/images/collageHpn.jpg" alt="collage of the band" className="collageBandPic"></img>
+        <img href="../src/assets/images/collageHpn.jpg" alt="collage of the band" className="collageBandPic"></img>
       </div>
       <div id="merch">
         <h1 className="sectionTitle">merch</h1>
