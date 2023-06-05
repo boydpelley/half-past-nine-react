e30a9e4c39171ba956074b1e4b422e099e9128e5
boydpelley <boydjrpelley@gmail.com>
2023-06-05 17:16:36 -0300

    make somewhat mobile compatable

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,18 @@
+import { useState, useEffect } from "react";
 import NavbarItem from "./NavbarItem";
 import "./Navbar.css";
 
 function Navbar() {
+    const [clicked, setClicked] = useState(false);
+
+
     return (
         <div className="nav">
             <h1 className="topName">hpn</h1>
-            <ul className="navList">
+            <div className="menuIcon" onClick={() => setClicked(!clicked)}>
+                <i className={clicked ? "xClose" : "hamburger"}></i>
+            </div>
+            <ul className={clicked ? "navList clicked" : "navList"}>
                 <NavbarItem section={"music"} />
                 <NavbarItem section={"media"} />
                 <NavbarItem section={"merch"} />
